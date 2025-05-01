a327a4be3ac0398bfb29aca0df578ed113d0eb3b
Siak Hooi <siakhooi@gmail.com>
2025-05-01 11:12:46 +0000

    update unit test

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,4 +1,8 @@
-from htmligator.util import get_folder_from_arguments
+from htmligator.util import (
+    get_folder_from_arguments,
+    get_file_object,
+    get_folder_object,
+)
 
 import pytest
 
@@ -18,3 +22,33 @@ def test_get_folder_from_arguments_many_values(monkeypatch):
         get_folder_from_arguments()
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 1
+
+
+def test_get_folder_from_arguments_one_value(monkeypatch):
+    monkeypatch.setattr("sys.argv", ["htmligator.py", "xx"])
+    assert get_folder_from_arguments() == "xx"
+
+
+def test_get_file_object():
+    name = "name.txt"
+    path = "xx/xx/name.txt"
+    assert get_file_object(name, path) == {
+        "name": name,
+        "path": path,
+        "type": "file",
+    }
+
+
+def test_get_folder_object():
+    name = "name"
+    path = "xx/xx/name"
+    children = [
+        {"name": "name1.txt", "path": "xx/xx/name/name1.txt", "type": "file"},
+        {"name": "name2.txt", "path": "xx/xx/name/name2.txt", "type": "file"},
+    ]
+    assert get_folder_object(name, path, children) == {
+        "name": name,
+        "path": path,
+        "type": "folder",
+        "children": children,
+    }
